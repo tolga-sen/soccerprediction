27dd092b6eabc97905a71b2dd90784a3937729cd
Paul Wlodkowski <pawlodkowski@gmail.com>
2019-09-11 17:14:25 +0200

    more testing

--- test_selenium_paul.py
+++ test_selenium_paul.py
@@ -2,11 +2,21 @@ from selenium import webdriver
 import time
 import numpy as np
 
-DRIVER = webdriver.Chrome()
+PAUL_PATH='/Users/PaulWlodkowski/Documents/PythonWork/\
+SPICED-Work/chromedriver/chromedriver'
+DRIVER = webdriver.Chrome(PAUL_PATH)
 
 def get_stats_from_window(driver, handle_number):
     driver.switch_to.window(handle_number)
     new_link = driver.current_url
+    # game_info = driver.find_element_by_xpath('//td[@id="flashscore_column"]').text
+    # game_info = '-'.join(game_info.split('\n')[0].split()[::2])
+    # print(game_info)
+    game_info = driver.find_elements_by_xpath('//span[@class="tname"]')
+    game_info = '-'.join([i.text for i in game_info])
+    print(f'TESTING!!!:{game_info}\n\n')
+
+
     statistics1=new_link[:-7]+"statistics;1"
     time.sleep(2)
 
@@ -15,10 +25,8 @@ def get_stats_from_window(driver, handle_number):
         time.sleep(2)
         login_form=driver.find_element_by_xpath('//div[@id="tab-statistics-1-statistic"]')
         statistics1=login_form.text
-        with open('half.txt', 'a') as f:
-            f.write('-----')
+        with open(f'game_data/half_{game_info}.txt', 'a') as f:
             f.write(statistics1)
-            f.write('-----')
 
         new_link = driver.current_url
         statistics2=new_link[:-7]+"statistics;0"
@@ -26,10 +34,8 @@ def get_stats_from_window(driver, handle_number):
         time.sleep(2)
         login_form=driver.find_element_by_xpath('//div[@id="tab-statistics-0-statistic"]')
         statistics2=login_form.text
-        with open('end.txt', 'a') as f:
-            f.write('-----')
+        with open(f'game_data/end_{game_info}.txt', 'a') as f:
             f.write(statistics2)
-            f.write('-----')
 
         new_link = driver.current_url
         goals=new_link[:-12]+"summary"
@@ -38,24 +44,20 @@ def get_stats_from_window(driver, handle_number):
         time.sleep(2)
         login_form=driver.find_element_by_xpath('//div[@id="summary-content"]')
         goals=login_form.text
-        with open('goal.txt', 'a') as f:
-            f.write('-----')
+        with open(f'game_data/goal_{game_info}.txt', 'a') as f:
             f.write(goals)
-            f.write('-----')
 
         new_link = driver.current_url
         info=new_link[:-12]+"summary"
 #         print(info)
-        time.sleep(3)
+        time.sleep(2)
         driver.get(info)
-        time.sleep(3)
+        time.sleep(2)
         login_form=driver.find_element_by_xpath('//td[@id="flashscore_column"]')
         info=login_form.text
 #         print(info)
-        with open('info.txt', 'a') as f:
-            f.write('-----')
+        with open(f'game_data/info_{game_info}.txt', 'a') as f:
             f.write(info)
-            f.write('-----')
 
     except:
         print("No Statistics for this game!!!")
@@ -66,10 +68,11 @@ if __name__ == '__main__':
     time.sleep(2)
     action = webdriver.common.action_chains.ActionChains(DRIVER)
     ELEMENTS = DRIVER.find_elements_by_class_name('padr')
+    ELEMENTS2 = DRIVER.find_elements_by_class_name('padl')
 
-    for elem in ELEMENTS:
-        time.sleep(3)
-        print(elem, elem.text)
+    for elem in ELEMENTS[:7]:
+        time.sleep(2)
+        # print(elem, elem.text)
         DRIVER.execute_script("arguments[0].scrollIntoView();", elem)
         DRIVER.execute_script("$(arguments[0]).click();", elem)
 
