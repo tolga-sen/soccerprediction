2996ff3b3447ead2d131cc681d7df9c86bc91397
tolga <alitolgasen@gmail.com>
2019-09-13 11:21:24 +0200

    changing the output name from end to endd

--- test_selenium_league.py
+++ test_selenium_league.py
@@ -31,7 +31,7 @@ def get_stats_from_window(driver, handle_number):
         time.sleep(2)
         login_form=driver.find_element_by_xpath('//div[@id="tab-statistics-1-statistic"]')
         statistics1=login_form.text
-        with open(f'game_data/eng1819/half_{game_info}.txt', 'a') as f:
+        with open(f'game_data/eng1920/half_{game_info}.txt', 'a') as f:
             f.write(statistics1)
 
         new_link = driver.current_url
@@ -40,7 +40,7 @@ def get_stats_from_window(driver, handle_number):
         time.sleep(2)
         login_form=driver.find_element_by_xpath('//div[@id="tab-statistics-0-statistic"]')
         statistics2=login_form.text
-        with open(f'game_data/eng1819/end_{game_info}.txt', 'a') as f:
+        with open(f'game_data/eng1920/endd_{game_info}.txt', 'a') as f:
             f.write(statistics2)
 
         new_link = driver.current_url
@@ -50,7 +50,7 @@ def get_stats_from_window(driver, handle_number):
         time.sleep(2)
         login_form=driver.find_element_by_xpath('//div[@id="summary-content"]')
         goals=login_form.text
-        with open(f'game_data/eng1819/goal_{game_info}.txt', 'a') as f:
+        with open(f'game_data/eng1920/goal_{game_info}.txt', 'a') as f:
             f.write(goals)
 
         new_link = driver.current_url
@@ -62,7 +62,7 @@ def get_stats_from_window(driver, handle_number):
         login_form=driver.find_element_by_xpath('//td[@id="flashscore_column"]')
         info=login_form.text
 #         print(info)
-        with open(f'game_data/eng1819/info_{game_info}.txt', 'a') as f:
+        with open(f'game_data/eng1920/info_{game_info}.txt', 'a') as f:
             f.write(info)
 
     except:
@@ -70,18 +70,22 @@ def get_stats_from_window(driver, handle_number):
 
 if __name__ == '__main__':
 
-    DRIVER.get("https://www.soccerstand.com/soccer/england/premier-league-2018-2019/results/")
+    DRIVER.get("https://www.soccerstand.com/soccer/england/premier-league/results/")
     time.sleep(2)
-    action = webdriver.common.action_chains.ActionChains(DRIVER)
-    showmore = DRIVER.find_element_by_xpath('//table[@class="link-more-games"]')
-    time.sleep(2)
-    count=4
-    while count >1:
-        showmore.location_once_scrolled_into_view
-        actions = ActionChains(DRIVER)
-        actions.click(showmore).perform()
-        time.sleep(2)
-        count-=1
+
+#   Action and showmore for the previous seasons where there is a button to load previous games !!
+#   if you are working with a season where all the games are load the page, you don't need 'showmore' part
+
+#    action = webdriver.common.action_chains.ActionChains(DRIVER)
+#    showmore = DRIVER.find_element_by_xpath('//table[@class="link-more-games"]')
+#    time.sleep(2)
+#    count=4
+#    while count >1:
+#        showmore.location_once_scrolled_into_view
+#        actions = ActionChains(DRIVER)
+#        actions.click(showmore).perform()
+#        time.sleep(2)
+#        count-=1
     ELEMENTS = DRIVER.find_elements_by_class_name('padr')
 
     for elem in ELEMENTS:
